2d9f6d4b9c9b487a72c05906694fa9bb7a75deef
asmaa <asmaa.haddad.2022@gmail.com>
2026-08-08 00:03:15 +0300

    feat : add Test

--- ExpenseFlow.Domain/Model/User/Role.cs
+++ ExpenseFlow.Domain/Model/User/Role.cs
@@ -6,5 +6,6 @@ public class Role : BaseModel
 {
     public string Name { get; set; } = string.Empty;
     public ICollection<User> Users { get; set; } = new List<User>();
+
     public ICollection<RolePermission> RolePermissions { get; set; } = new List<RolePermission>();
 }
