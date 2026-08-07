8730245b7ceade61ce39c78b8e1125b3f70a482e
asmaa <asmaa.haddad.2022@gmail.com>
2026-08-07 23:35:56 +0300

    feat : Add

--- ExpenseFlow.Domain/Model/User/RolePermission.cs
+++ ExpenseFlow.Domain/Model/User/RolePermission.cs
@@ -7,6 +7,13 @@ public class RolePermission : BaseModel
     public long RoleId { get; set; }
     public Role Role { get; set; }
 
+
+
+
+
+
+
+
     public long PermissionId { get; set; }
     public Permission Permission { get; set; }
 }
